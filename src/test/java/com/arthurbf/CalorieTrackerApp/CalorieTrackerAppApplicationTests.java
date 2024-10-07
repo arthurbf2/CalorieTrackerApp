7923d3c9f34f761bffbcea846307034a874f937f
arthurbf2 <arthurfalcao2010@gmail.com>
2024-10-07 16:11:59 -0300

    init repo

---
+++ src/test/java/com/arthurbf/CalorieTrackerApp/CalorieTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurbf.CalorieTrackerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
