370e75fb16f9a84ecd585219ba8408ae504fa5de
arthurbf2 <arthurfalcao2010@gmail.com>
2024-10-21 14:57:50 -0300

    add toastrService

--- src/frontend/src/app/pages/login/login.component.ts
+++ src/frontend/src/app/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -21,7 +22,7 @@ import { LoginService } from '../../services/login.service';
 })
 export class LoginComponent {
   loginForm!: FormGroup;
-  constructor(private router: Router, private loginService: LoginService) {
+  constructor(private router: Router, private loginService: LoginService, private toastService: ToastrService) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
@@ -29,8 +30,8 @@ export class LoginComponent {
   }
   submit(){
     this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next: () => console.log("success"),
-      error: () => console.log("error")
+      next: () => this.toastService.success("Login successful."),
+      error: () => this.toastService.error("Unexpected error, try again later.")
     })
   }
 
